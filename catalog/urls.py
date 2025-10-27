bf66db311d04068f8b4732d93f432a8c23d6d80b
Oksana Konkina <oksanka.808@mail.ru>
2025-10-27 19:51:38 +0300

    POST-request added

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from catalog.apps import CatalogConfig
 from catalog.views import home
 from catalog.views import contacts
+from catalog.views import answer
+
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('answer/', answer, name='answer')
 ]
