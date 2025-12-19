186809c2d4d2f5a23584f410e79c35f08acdd232
Oksana Konkina <oksanka.808@mail.ru>
2025-12-19 19:40:12 +0300

    added form classes, validations, delivery_option_is(booleanfield)

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,18 @@ from django.forms import ModelForm
 
 from blog.models import Post
 
+forbidden_words = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
+
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
@@ -10,8 +22,14 @@ class StyleFormMixin:
         for field_name, field in self.fields.items():
             if isinstance(field, BooleanField):
                 field.widget.attrs["class"] = "form-check-input"
+
             else:
                 field.widget.attrs["class"] = "form-control"
+
+                # Используем help_text как placeholder для текстовых полей
+                if field.help_text:
+                    field.widget.attrs["placeholder"] = field.help_text
+                    field.help_text = ""
 
 
 class PostForm(StyleFormMixin, ModelForm):
@@ -23,18 +41,6 @@ class PostForm(StyleFormMixin, ModelForm):
         cleaned_data = super().clean()
         title = cleaned_data.get("title").lower()
         content = cleaned_data.get("content").lower()
-
-        forbidden_words = [
-            "казино",
-            "криптовалюта",
-            "крипта",
-            "биржа",
-            "дешево",
-            "бесплатно",
-            "обман",
-            "полиция",
-            "радар",
-        ]
 
         for word in forbidden_words:
             if word in title:
--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,4 @@
-from django.db.models import BooleanField
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField, CheckboxInput, FileInput
 from django.core.exceptions import ValidationError
 
 from catalog.models import Product
@@ -21,10 +20,20 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            if isinstance(field, BooleanField):
+            if isinstance(field, BooleanField) or isinstance(
+                field.widget, CheckboxInput
+            ):
                 field.widget.attrs["class"] = "form-check-input"
+
+            elif isinstance(field.widget, FileInput):
+                field.widget.attrs["class"] = "form-control"
+
             else:
                 field.widget.attrs["class"] = "form-control"
+                # Используем help_text как placeholder для текстовых полей
+                if field.help_text:
+                    field.widget.attrs["placeholder"] = field.help_text
+                    field.help_text = ""
 
 
 class ProductForm(StyleFormMixin, ModelForm):
@@ -38,8 +47,10 @@ class ProductForm(StyleFormMixin, ModelForm):
 
         for word in forbidden_words:
             if word in name_lower:
-                raise ValidationError("Выбранное вами слово запрещено для использования.")
-        
+                raise ValidationError(
+                    "Выбранное вами слово запрещено для использования."
+                )
+
         return name
 
     def clean_description(self):
@@ -48,8 +59,10 @@ class ProductForm(StyleFormMixin, ModelForm):
 
         for word in forbidden_words:
             if word in description_lower:
-                raise ValidationError("Выбранное вами слово запрещено для использования.")
-        
+                raise ValidationError(
+                    "Выбранное вами слово запрещено для использования."
+                )
+
         return description
 
     def clean_price(self):
