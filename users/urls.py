ccae60b53553b78efda601ad46f9fd9b06289425
Oksana Konkina <oksanka.808@mail.ru>
2025-12-23 19:40:16 +0300

    added new app, registration, authentication, forms,migrations

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.urls import path
+from users.apps import UsersConfig
+from django.contrib.auth.views import LoginView, LogoutView
+
+from users.views import UserCreateView, email_verification, UserUpdateView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
+    path("change-user/<str:token>/", UserUpdateView.as_view(), name="change-user")
+]
